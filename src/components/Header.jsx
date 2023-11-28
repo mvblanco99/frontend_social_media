c7326098ba677ad7677802eab3bf998c661d1c25
mvblanco99 <manuelvalentin1999@gmail.com>
2023-11-27 23:38:16 -0400

    Aplicando cambios a los estilos del componente header y home para hacerlo adaptable a distintos dispositivos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,77 @@
 import { Link } from 'react-router-dom'
 import stylesHeader from './Header.module.css'
 import SearchIcon from '@mui/icons-material/Search';
+import { useSelector } from 'react-redux';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => {
+
+  const userState = useSelector(state => state.users)
+  const { img, name, lastname } = userState.user
+
   return (
     <>
-        <div className={stylesHeader.header}>
-            <div className={stylesHeader.container_links}>
-              <Link to='/home'>Home</Link>
-              <Link to='/profile'>Profile</Link>
-              <a href="#">Notifications</a>
-            </div>
-
-            <div className={stylesHeader.container_search_img}>
-              <form className={stylesHeader.form}> 
-                <label htmlFor="">
-                  <input 
-                    type="text" 
-                    placeholder="Search"
-                    className={stylesHeader.input_text}
-                  />
-                  <button type='button'><SearchIcon/></button>
-                </label>
-              </form>
-              
-              <button 
-                type='button'
-                className={stylesHeader.button}
-                >
-                <img src="https://scontent.fmrd1-1.fna.fbcdn.net/v/t1.6435-9/121486890_3499566146791612_7447384720159423732_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=be3454&_nc_ohc=DL7Vwgw-ak0AX_b3sKf&_nc_ht=scontent.fmrd1-1.fna&oh=00_AfCcwHG5xkBvAjildKeyk0EVIid8YiaS4IDauY7w2e7Cpw&oe=65802E13" alt="img-profile" />
-              </button>
-
-            </div>
+      <div className={stylesHeader.header}>
+        
+        <div className={stylesHeader.logo}>
+          <img src={img} alt={`${name} ${lastname}`} />
         </div>
+
+        <div className={stylesHeader.container_icons}>
+          
+          <Link to='/home'> 
+            <span className={stylesHeader.icon_responsive}>
+              <HomeIcon/>
+            </span>
+            <span className={stylesHeader.text_responsive}>
+              Home
+            </span>
+          </Link>
+
+          <Link to='/profile'>
+            <span className={stylesHeader.icon_responsive}>
+              <AccountBoxIcon/>
+            </span>
+            <span className={stylesHeader.text_responsive}>
+            Profile
+            </span>
+          </Link>
+
+          <Link to='#'>
+            <span className={stylesHeader.icon_responsive}>
+              <NotificationsIcon/>
+            </span>
+            <span className={stylesHeader.text_responsive}>
+            Notifications
+            </span>
+          </Link>
+        
+          <form className={stylesHeader.form}> 
+            <label htmlFor="">
+              <input 
+                type="text" 
+                placeholder="Search"
+                className={stylesHeader.input_text}
+              />
+              <button type='button'><SearchIcon/></button>
+            </label>
+          </form>
+          
+        </div>
+
+        <div className={stylesHeader.container_button_session}>
+          <button 
+              type='button'
+              className={stylesHeader.button_session}
+              >
+              <img src={img} alt={`${name} ${lastname}`} />
+          </button>
+        </div>
+        
+      </div>
     </>
   )
 }
