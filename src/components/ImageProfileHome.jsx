1eaade1c44925e5e88a848e5e4d2f4ca9b105d20
mvblanco99 <manuelvalentin1999@gmail.com>
2023-12-06 23:27:39 -0400

    Estilando el ImageProfileHome

--- src/components/ImageProfileHome.jsx
+++ src/components/ImageProfileHome.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
 import stylesImageProfileHome from './ImageProfileHome.module.css'
 import { useSelector } from 'react-redux'
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import EditIcon from '@mui/icons-material/Edit';
+import { Badge, IconButton } from '@mui/material';
+
 
 const ImageProfileHome = () => {
 
   const userState = useSelector(state => state.users)
-    const { img, name, lastname, userId } = userState.user
+    const { img, name, lastname } = userState.user
 
   return (
     <div className={stylesImageProfileHome.container}>
+      
       <div className={stylesImageProfileHome.cover_image}>
         <img src="https://criticalhits.com.br/wp-content/uploads/2021/11/rengoku-1.jpg" alt="MiamorRengoku" />
       </div>
+
       <div className={stylesImageProfileHome.container_profile_img}>
         <img src={img} alt={`${name} ${lastname}`} />
       </div>
+
+      <div className={stylesImageProfileHome.icon_edit_image_profile}>
+        <IconButton >
+          <Badge  color="primary">
+            <CameraAltIcon/>
+          </Badge>
+        </IconButton> 
+      </div>
+
       <div className={stylesImageProfileHome.container_description}>
-        <div>
+        <div className={stylesImageProfileHome.sub_container_description}>
           <h1>{name} {lastname}</h1>
           <p>Hola mi nombre es Manuel Blanco y soy Ingeniero en informatica, 
           me gusta la programacion, el boxeo y el cafe</p>
         </div>
+        <div className={stylesImageProfileHome.icon_edit_description}>
+          <div>
+            <IconButton >
+              <Badge  color="primary">
+                <EditIcon/>
+              </Badge>
+            </IconButton>
+          </div>
+        </div>
       </div>
+
     </div>
   )
 }
