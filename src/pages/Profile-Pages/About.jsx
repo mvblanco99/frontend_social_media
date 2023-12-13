74bb0c74f8cf30d60631594eb8d6aa747e6b1cf5
GABB1t0 <antuarez18@gmail.com>
2023-12-13 12:18:34 -0400

    fixing section in about

--- src/pages/Profile-Pages/About.jsx
+++ src/pages/Profile-Pages/About.jsx
@@ -4,18 +4,41 @@ import Photos from './Photos';
 import Friends from './Friends';
 import SectionSeparator from '../../components/Profile/SectionSeparator';
 import { useState } from 'react';
+import { object } from 'prop-types';
 
 const About = () =>{
 
-  const [data, setData] = useState({'':''});
+  const [data, setData] = useState({
+    'About me': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',
+  });
 
   const info = {
-    'About me': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',  
+    'About me': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',
+
+    'Work':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',
+    
+    'Education': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',
+
+    'Family': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!',
+
+    'contact': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo earum totam, deserunt ullam debitis saepe assumenda, repellat eum facilis, corrupti dicta nam! Magnam dolore alias, odit non asperiores nam exercitationem!'
   };
 
   const handleClick = (item) =>{
-    return (setData(info[item]));
+    return (setData({ [item]: info[item] }));
   };
+
+  const getEntries = () =>{
+    
+    let keys = Object.keys(data);
+    console.log(keys);
+
+    let values = Object.values(data);
+    console.log(values);
+
+    return [keys, values];
+  };
+  const [key,val] = getEntries();
 
   return(
     <>
@@ -23,16 +46,17 @@ const About = () =>{
         <nav className={stylesAbout.menuOption}>
           <ul>
             <li onClick={()=>handleClick('About me')}>Something About Me</li>
-            <li>Working Zone</li>
-            <li>Educational Qualification</li>
-            <li>Work & Education</li>
-            <li>Friends & Family</li>
-            <li>Contact Details</li>
+            <li onClick={()=>handleClick('Work')}>Working Zone</li>
+            <li onClick={()=>handleClick('Education')}>Educational Qualification</li>
+            
+            <li onClick={()=>handleClick('Family')}>Friends & Family</li>
+            <li onClick={()=>handleClick('contact')}>Contact Details</li>
           </ul>
         </nav>
         <article className={stylesAbout.infoAbout}>
-          <h3>{data}</h3>
-          <p>{data}</p>
+          
+          <h3>{key.map(key => key)}</h3>
+          <p>{val.map(val=>val)}</p>
                     
         </article>
       </section>
